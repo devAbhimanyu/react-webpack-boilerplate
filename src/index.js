82c0180552921d8b8a1a52d6eb12813dfee28f73
Abhimanyu Chauhan <dev.chauhan.singh.abhimanyu@gmail.com>
2019-03-11 15:33:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
-import App from './container/App';
-import './main.css';
+import App from "./container/App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
