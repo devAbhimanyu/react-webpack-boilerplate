efa12055038f394d24c6ec2c582945acad3fa225
devabhimanyu <dev.chauhan.singh.abhimanyu@gmail.com>
2020-03-04 00:41:01 +0530

    counter-app

--- src/container/App.js
+++ src/container/App.js
@@ -1,15 +1,46 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+// import { increment, decrement } from "../features/counter/counter";
+import { increment, decrement } from "../features/counter/counterSlice";
+import { Box, Card, Heading, Button, Flex } from "rebass";
 
 class App extends Component {
-  state = {
-    value: "this was created without using create-react-app"
-  };
   render() {
     return (
-      <h1>
-        {this.state.value}
-      </h1>
+      <Flex justifyContent={"center"}>
+        <Box width={256} mt={10}>
+          <Card
+            sx={{
+              p: 1,
+              borderRadius: 2,
+              boxShadow: "0 0 16px rgba(0, 0, 0, .25)",
+              textAlign: "center"
+            }}
+          >
+            <Box px={2}>
+              <Heading as="h1">{this.props.value}</Heading>
+              <Button
+                sx={{ color: "#000" }}
+                variant="primary"
+                mr={2}
+                onClick={this.props.increment}
+              >
+                add
+              </Button>
+              <Button sx={{ color: "#000" }} onClick={this.props.decrement}>
+                decre
+              </Button>
+            </Box>
+          </Card>
+        </Box>
+      </Flex>
     );
   }
 }
-export default App;
+
+const mapStateToProps = state => ({
+  value: state.value
+});
+
+const mapDispatchToProps = { increment, decrement };
+export default connect(mapStateToProps, mapDispatchToProps)(App);
