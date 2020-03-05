5e3d4b4dc9c52d5ea1a843da057951f832238c8e
devabhimanyu <dev.chauhan.singh.abhimanyu@gmail.com>
2020-03-05 14:15:51 +0530

    original

--- src/container/App.js
+++ src/container/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-// import { increment, decrement } from "../features/counter/counter";
-import { increment, decrement } from "../features/counter/counterSlice";
-import { Box, Card, Heading, Button, Flex } from "rebass";
 
 class App extends Component {
+  state = {
+    value: "this was created without using create-react-app"
+  };
   render() {
-    return <h1>{this.state.value}</h1>;
+    return (
+      <h1>
+        {this.state.value}
+      </h1>
+    );
   }
 }
-
-const mapStateToProps = state => ({
-  value: state.value
-});
-
-const mapDispatchToProps = { increment, decrement };
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./container/App";
 import "./index.css";
 
-const store = configureStore({
-  reducer: rootReducer
-});
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
